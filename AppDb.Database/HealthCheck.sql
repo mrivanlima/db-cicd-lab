5829be1f65a69220861a0e0f79aba33855720bf8
MRIvanLima <mrivanlima@gmail.com>
2026-02-13 15:32:05 -0500

    Baseline: SSDT project + Docker env + docs

--- AppDb.Database/HealthCheck.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[HealthCheck]
-(
-    [HealthCheckId] INT IDENTITY(1,1) NOT NULL,
-    [CreatedOn]      DATETIME2(0) NOT NULL
-        CONSTRAINT [DF_HealthCheck_CreatedOn] DEFAULT (SYSUTCDATETIME()),
-    CONSTRAINT [PK_HealthCheck] PRIMARY KEY CLUSTERED ([HealthCheckId] ASC)
-);
