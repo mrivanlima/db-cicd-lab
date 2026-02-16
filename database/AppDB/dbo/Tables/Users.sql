a1ee4a81f719e306cae2a124c37d48fd976ebd8b
MRIvanLima <mrivanlima@gmail.com>
2026-02-16 11:03:30 -0500

    AB#2

---
+++ database/AppDB/dbo/Tables/Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [UserId]    INT            NOT NULL,
+    [Email]     NVARCHAR (255) NOT NULL,
+    [CreatedAt] DATETIME2 (7)  CONSTRAINT [DF_Users_CreatedAt] DEFAULT (sysdatetime()) NOT NULL,
+    [MyColumn]  INT            NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([UserId] ASC)
+);
+
