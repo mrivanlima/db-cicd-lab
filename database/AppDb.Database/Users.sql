09ae8b6fd101aedf3148bb43854a95a37c829128
MRIvanLima <mrivanlima@gmail.com>
2026-02-16 09:23:33 -0500

    Pr for my column

--- database/AppDb.Database/Users.sql
+++ database/AppDb.Database/Users.sql
@@ -3,7 +3,6 @@
     [UserId] INT NOT NULL,
     [Email] NVARCHAR(255) NOT NULL,
     [CreatedAt] DATETIME2 NOT NULL CONSTRAINT DF_Users_CreatedAt DEFAULT (SYSDATETIME()),
-    [UpdatedBy]  DATETIME2 NOT NULL,
-
+    [MyColumn] int,
     CONSTRAINT PK_Users PRIMARY KEY CLUSTERED ([UserId])
 );
