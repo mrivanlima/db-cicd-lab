a6d4cd77a3f9131171e405d4930198f404301bcc
MRIvanLima <mrivanlima@gmail.com>
2026-02-16 09:55:24 -0500

    Another Column

--- database/AppDb.Database/Users.sql
+++ database/AppDb.Database/Users.sql
@@ -4,5 +4,6 @@
     [Email] NVARCHAR(255) NOT NULL,
     [CreatedAt] DATETIME2 NOT NULL CONSTRAINT DF_Users_CreatedAt DEFAULT (SYSDATETIME()),
     [MyColumn] int,
+    [AnotherColumn] int,
     CONSTRAINT PK_Users PRIMARY KEY CLUSTERED ([UserId])
 );
